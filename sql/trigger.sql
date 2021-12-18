14cad6635700204cfc8f1d4c4262d2ff8f1221ea
Zifan Zhu <55147873+SleepB411@users.noreply.github.com>
2021-12-18 16:12:13 -0500

    Update trigger.sql

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,21 +1,3 @@
-create function mytrigger()
-	returns trigger as
-$body$
-begin
-if NEW.in_stock < 10 then
-	update book
-	set in_stock = in_stock+(select sum(quantity) 
-							 from user_order 
-							 where id =NEW.id 
-							 and order_date < current_timestamp 
-							 and order_date > current_timestamp - interval '720 hours')
-	where book.id = NEW.id;
-end if;
-return NEW;
-end;
-$body$
-	language plpgsql volatile;
-
 CREATE TRIGGER restock
     AFTER UPDATE ON book
     FOR EACH ROW 
